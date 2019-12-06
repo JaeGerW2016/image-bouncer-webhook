4bd124288bc74599e2d64ad34a6e25acad85693c
jasper <314315960@qq.com>
2019-12-06 17:03:38 +0800

    add test

--- deployment/patch-ca-bundle.sh
+++ deployment/patch-ca-bundle.sh
@@ -8,27 +8,6 @@ set -o pipefail
 
 export CA_BUNDLE=$(kubectl get configmap -n kube-system extension-apiserver-authentication -o=jsonpath='{.data.client-ca-file}' | base64 | tr -d '\n')
 
-cat <<EOF  >> ../k8s/mutatingwebhook-ca-bundle.yaml
-apiVersion: admissionregistration.k8s.io/v1beta1
-kind: MutatingWebhookConfiguration
-metadata:
-  name: image-bouncer-mutate
-webhooks:
-  - name: image-bouncer-mutate.webhook.io
-    clientConfig:
-      service:
-        name: image-bouncer-webhook-service
-        namespace: default
-        path: "/mutate"
-      caBundle: ${CA_BUNDLE}
-    failurePolicy: Ignore
-    rules:
-      - operations: [ "CREATE" ]
-        apiGroups: [""]
-        apiVersions: ["v1"]
-        resources: ["pods"]
-EOF
-
 cat <<EOF  >> ../k8s/validatingwebhook-ca-bundle.yaml
 apiVersion: admissionregistration.k8s.io/v1beta1
 kind: ValidatingWebhookConfiguration
